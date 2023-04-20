fcedf17aac5cec3c4b1bb3c5b5104f521cca32ac
Luca Diegel <Luca@trenz.de>
2023-04-20 17:53:19 +0200

    added files to git

---
+++ src/main/java/UNI/MovieAPI/MovieAPiApplication.java
@@ -0,0 +1,13 @@
+package UNI.MovieAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieAPiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieAPiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/UNI/MovieAPI/MovieAPiApplicationTests.java
@@ -0,0 +1,13 @@
+package UNI.MovieAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieAPiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
